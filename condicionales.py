e7b226850b673434e5bad27eaa5b5b89aaaf274e
BryanJaime <bjaimed@unal.edu.co>
2025-09-01 20:38:30 -0500

    Se compara para ver si el numero ingresado es menor que 5

--- condicionales.py
+++ condicionales.py
@@ -1,7 +1,12 @@
 # Almacenamos un dato ingresado por consola en una variable
 A = float(input("Ingrese un numero para evaluarlo: "))
-# Evaluamos el dato ingresado
+# Evaluamos el dato ingresado para ver si es mayor que 5
 if A > 5:
     print(f"El numero {A} es mayor que 5")
 else:
     print(f"El numero {A} no es mayor que 5")
+# Evaluamos el dato ingresado para ver si es menor que 5
+if A < 5:
+    print(f"El numero {A} es menor que 5")
+else:
+    print(f"El numero {A} no es menor que 5")
