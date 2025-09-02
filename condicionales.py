79cd0cbabb07335d9c899d21ba8f4d6e5ab0960b
BryanJaime <bjaimed@unal.edu.co>
2025-09-01 20:29:50 -0500

    Uso de condicionales para evaluar si un numero dado es mayor que 5

---
+++ condicionales.py
@@ -0,0 +1,7 @@
+# Almacenamos un dato ingresado por consola en una variable
+A = float(input("Ingrese un numero para evaluarlo: "))
+# Evaluamos el dato ingresado
+if A > 5:
+    print(f"El numero {A} es mayor que 5")
+else:
+    print(f"El numero {A} no es mayor que 5")
