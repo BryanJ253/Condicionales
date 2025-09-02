5d0902e332d4cc5810b01c064cacd829b4def350
BryanJaime <bjaimed@unal.edu.co>
2025-09-01 20:44:46 -0500

    Se agrega la comparacion "<="

--- condicionales.py
+++ condicionales.py
@@ -15,3 +15,8 @@ if A >= 5:
     print(f"El numero {A} es mayor o igual que 5")
 else:
     print(f"El numero {A} no es mayor ni igual a 5")
+# Evaluamos el dato ingresado para ver si es menor o igual que 5
+if A <= 5:
+    print(f"El numero {A} es menor o igual que 5")
+else:
+    print(f"El numero {A} no es menor ni igual a 5")
